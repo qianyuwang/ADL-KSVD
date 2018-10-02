ab37f7cd63905718b4879033247bf91cc09fb82f
Qianyu Wang <47380786@qq.com>
2018-10-02 14:40:16 +0800

    Update main.m

--- main.m
+++ main.m
@@ -37,9 +37,9 @@ for i=1:size(par.sparsitythres,2)
             %initialization
             fprintf('\nADL-KSVD Initializing ... ...');
             tic
-            [Htr_Extend]=extend_H( H_train,Magni_H );
-            [Xinit,Ominit,Winit]=INIT(train_feats,H_train,Htr_Extend,alpha,delta,beta);
-            time4init=toc;
+            [Htr_Extend]=extend_H( H_train,Magni_H ); %initialize H
+            [Xinit,Ominit,Winit]=INIT(train_feats,H_train,Htr_Extend,alpha,delta,beta); % initialize X, Omega and W
+            time4init=toc; % record time
             init.X=[init.X;Xinit];
             init.Om=[init.Om;Ominit];
             init.W=[init.W;Winit];
@@ -48,14 +48,14 @@ for i=1:size(par.sparsitythres,2)
             %training
             fprintf('\nADL-KSVD Training ... ...');
             tic
-            [X,Om,W]=TRAIN(train_feats,H_train,Xinit,Ominit,Winit,alpha,beta,iterations,itecircles,sparsitythres);
+            [X,Om,W]=TRAIN(train_feats,H_train,Xinit,Ominit,Winit,alpha,beta,iterations,itecircles,sparsitythres); %training
             time4train=toc;
             fprintf('Time for training is: %.03f seconds.\n',time4train);
             
             %classification
             fprintf('\nADL-KSVD CLASSIFICATION ... ...');
             tic
-            [~,accuracy]=CLASS(test_feats,H_test,Om,W,sparsitythres);
+            [~,accuracy]=CLASS(test_feats,H_test,Om,W,sparsitythres); %testing
             time4class=toc;
             fprintf('Time for classification is: %.03f seconds.\n',time4class);
             fprintf('Accuracy for classification is: %.02f%%.\n',accuracy*100);
